3f00eb9967c9eb9aa21b4b387d05dbba258a80bc
Samir Costa <samir.coutinho@fortaleza.ce.gov.br>
2016-08-29 13:31:42 -0300

    add lang PT_BR

---
+++ lang/PT_BR.inc.php
@@ -0,0 +1,67 @@
+<?php
+/**universal
+ * Brasilian Portuguese language translation
+ * 
+ * @author Daniel Lawson
+ * @author Thomas Pequet
+ * @author Tiago N. Sampaio 05/01/2005
+ * @version 1.0
+ */
+
+$titre1		= "Estatisticas de utilização do CUPS";
+ 
+// Nom des jours de la semaine
+$nomJours = array(
+                  "0" => "Domingo",
+                  "1" => "Segunda",
+                  "2" => "Terça",
+                  "3" => "Quarta",
+                  "4" => "Quinta",
+                  "5" => "Sexta",
+                  "6" => "Sabado"																			
+              );
+// Nom des mois
+$nomMois = array(
+                  "01" => "Janeiro",
+                  "02" => "Fevereiro",
+                  "03" => "Março",
+                  "04" => "Abril",
+                  "05" => "Maio",
+                  "06" => "Junho",
+                  "07" => "Julho",				
+                  "08" => "Augosto",
+                  "09" => "Setembro",
+                  "10" => "Outubro",
+                  "11" => "Novembro",
+                  "12" => "Dezembro"
+              ); 
+ 
+// Texte de la page "index"
+if ($page=="1") {
+		$ficIndex1 		= "Numero de Paginas por Usuário";
+		$ficIndex2 		= "Numero de Paginas por Impressora";
+		$ficIndex3 		= "Numero de paginas por Dia";
+		$ficIndex4 		= "Numero de Paginas por Hora";
+		$ficIndex5 		= "Estatisticas do Usuario: ##utilisateur##";
+		$ficIndex6 		= "Estatisticas da impressora: ##imprimante##";
+		$ficIndex7 		= "Estatisticas do Serviço: ##service##";
+		$ficIndex8 		= "Veja Informações do CUPS";
+		$ficIndex9 		= "Numero de paginas impressas";
+		$ficIndex10		= "Custo";	
+		$ficIndex11 		= "Numero de paginas por serviço";
+		$ficIndex12 		= "de ##date1## até ##date2##";
+		$ficIndex13 		= "Lista de Serviços";
+		$ficIndex14		= "Numero de paginas por Mes";
+		$ficIndex15		= "Total de paginas impressas + custo";
+		$ficIndex16 		= "Usuarios";
+		$ficIndex17 		= "% Paginas";
+		$ficIndex18 		= "Numero de Paginas";
+		$ficIndex19 		= "Impressora";
+		$ficIndex20		= "custo em R$";
+		$ficIndex21		= "Custo das paginas em R$";
+		$ficIndex22		= "Atualizar do Log";
+		$ficIndex23			= "Période à consulter";
+		$ficIndex24			= "Valider";
+		$ficIndex25			= "Modifier";   	
+} 
+?>
